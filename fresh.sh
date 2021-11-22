ce0d332c711e16babc5c169e92173d69cc95b5e6
Dries Vints <dries@vints.io>
2021-11-22 15:45:31 +0100

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -12,6 +12,10 @@ echo "Setting up your Mac..."
 #  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 #fi
 
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
 # Update Homebrew recipes
 #brew update
 
@@ -27,10 +31,6 @@ mkdir $HOME/Coding/crometrics
 
 # Clone Github repositories
 ./clone.sh
-
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
