980517bfc186c53291ced81e4015d96a09282a6d
Dries Vints <dries@vints.io>
2022-02-21 15:48:57 +0100

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -2,16 +2,16 @@
 
 echo "Cloning repositories..."
 
-CODING=$HOME/Coding
+CODING=$HOME/Code
 CRO=$CODING/crometrics
 
 # Personal
-git@github.com:ryanzigler/cobrabot.git ~/Coding
-git@github.com:ryanzigler/falls-massage.git ~/Coding
-git@github.com:ryanzigler/leetCode-toy-problems.git ~/Coding
-git@github.com:ryanzigler/zigler.xyz.git ~/Coding
+git@github.com:ryanzigler/cobrabot.git ~/Code
+git@github.com:ryanzigler/falls-massage.git ~/Code
+git@github.com:ryanzigler/leetCode-toy-problems.git ~/Code
+git@github.com:ryanzigler/zigler.xyz.git ~/Code
 
 # Cro Metrics
-git@github.com:CROmetrics/crometrics-experiments.git ~/Coding/crometrics
-git@github.com:CROmetrics/crometrics-experiments.git ~/Coding/crometrics
-git@github.com:CROmetrics/oli-chrome.git ~/Coding/crometrics
+git@github.com:CROmetrics/crometrics-experiments.git ~/Code/crometrics
+git@github.com:CROmetrics/crometrics-experiments.git ~/Code/crometrics
+git@github.com:CROmetrics/oli-chrome.git ~/Code/crometrics
